21f3837f5188b97657a039e12931aeca0fc9ddb8
bastienbonnet-ladregarde <bastien.bonnet--ladregarde@edu.devinci.fr>
2026-04-04 18:14:44 +0200

    Update Moniteur.cs

--- ProjetAutoEcoleS4/ProjetAutoEcoleS4/Models/Moniteur.cs
+++ ProjetAutoEcoleS4/ProjetAutoEcoleS4/Models/Moniteur.cs
@@ -8,44 +8,44 @@ namespace ProjetAutoEcoleS4.Models
 {
     internal class Moniteur
     {
-        private static int compteurId=0;
-        public int id_Moniteur { get; set; }//Clé primaire
+        private static int compteurId = 0;
+        public int id_moniteur { get; set; }//Clé primaire
         public string nom { get; set; }
         public string prenom { get; set; } //Auto ou manuelle
-        public string permis_moniteur { get; set; }
-        public int salaire_Moniteur { get; set; }
+        public string permisMoniteur { get; set; }
+        public int salaireMoniteur { get; set; }
 
         public Moniteur(string nom)
         {
             
-            this.id_Moniteur = compteurId;
+            this.id_moniteur = compteurId;
             compteurId ++;
             this.nom = nom;
             this.prenom = "";
-            this.permis_moniteur = "";
-            this.salaire_Moniteur = 0;
+            this.permisMoniteur = "";
+            this.salaireMoniteur = 0;
         }
         public Moniteur()
         {
-            id_Moniteur = compteurId;
+            id_moniteur = compteurId;
             compteurId++;
             nom = "";
             prenom = "";
-            permis_moniteur = "";
-            salaire_Moniteur = 0;
+            permisMoniteur = "";
+            salaireMoniteur = 0;
         }
             static public bool operator  ==(Moniteur e1,Moniteur e2)
         {
-            return e1.id_Moniteur==e2.id_Moniteur;
+            return e1.id_moniteur==e2.id_moniteur;
         }
         static public bool operator  !=(Moniteur e1,Moniteur e2)
         {
-            return e1.id_Moniteur!=e2.id_Moniteur;
+            return e1.id_moniteur!=e2.id_moniteur;
         }
 
         public override string ToString()
         {
-            return $"{id_Moniteur} | Nom: {nom} | Prénom: {prenom}";
+            return $"{id_moniteur} | Nom: {nom} | Prénom: {prenom}";
         }
     }
 }
