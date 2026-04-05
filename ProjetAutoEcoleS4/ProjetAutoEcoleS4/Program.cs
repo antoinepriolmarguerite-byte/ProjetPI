041f7100a028f753d6d8d94305af80c6b8e08f4b
bastienbonnet-ladregarde <bastien.bonnet--ladregarde@edu.devinci.fr>
2026-04-05 23:09:34 +0200

    comm

--- ProjetAutoEcoleS4/ProjetAutoEcoleS4/Program.cs
+++ ProjetAutoEcoleS4/ProjetAutoEcoleS4/Program.cs
@@ -5,6 +5,14 @@ using ProjetAutoEcoleS4.Interfaces;
 using ProjetAutoEcoleS4.Models;
 using ProjetAutoEcoleS4.Services;
 
+//Bonjour, bienvenue dans notre projet de gestion d'auto-école !
+//Nous avons tenu à réaliser ce projet avec le minimun d'utilisation de l'IA
+// 95% du code que vous voyez n'a jamais vu une ligne d'IA.
+// L'IA a été utilisée pour nous aider à corriger les bugs que nous avons rencontrés, 
+// et pour nous aider à trouver des solutions à certains problèmes sur lesquels nous n'avions pas encore été formés
+// Nous sommes conscient que ce projet n'est pas parfait, mais nous avons fait de notre mieux pour respecter les consignes 
+// et pour fournir un projet fonctionnel et complet à temps.
+
 Console.WriteLine("========== CONFIGURATION BDD ===========");
 Console.Write("Port (3306 par défaut) : ");
 string port = Console.ReadLine();
